353c24e22c002b580c6f268f748eb1f445a69e7c
Katie Tsui <katietsui@katietsui.myfiosgateway.com>
2020-05-08 08:27:01 -0400

    added datamining section on COVID-19 statistics and added links to home page

--- app.py
+++ app.py
@@ -7,11 +7,15 @@
 
 #import statements
 from flask import Flask, render_template
+import requests
+import bs4
+import pandas as pd
+import numpy as np
 
 #Flask app variable
 app = Flask(__name__)
 
-#static route
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -25,6 +29,15 @@ def assignment():
 @app.route("/course")
 def course():
     return render_template("course.html")
+
+#datamining route
+@app.route("/data")
+def datamining():
+    bs=bs4.BeautifulSoup(requests.get('https://www.worldometers.info/coronavirus/').content, 'lxml')
+    tables=bs.find_all('table')
+    main_table=pd.read_html(str(tables[0]))[0]
+    main_table = main_table.replace(np.nan, '', regex=True)
+    return render_template('data.html', tables=[main_table.to_html()])
 
 #start the server
 if __name__ == "__main__":
